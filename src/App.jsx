013d7971c31d9950485d5c840402bbda2e5b07a6
RoMan-robots <romkozhan@gmail.com>
2025-05-15 18:57:56 +0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,41 @@
+import { useState } from 'react'
+import * as h from './components/Handle'
+import './App.css'
+
+function App() {
+  const [num1, setNum1] = useState('')
+  const [num2, setNum2] = useState('')
+  const [result, setResult] = useState(0)
+
+  function clear () {
+    setNum1('')
+    setNum2('')
+    setResult(0)
+  }
+
+  return (
+    <>
+      <h1>React Calculator</h1>
+      <h3>Created by RoMan</h3>
+      <input 
+        type="number" 
+        value={num1}
+        onChange={(e) => setNum1(e.target.value)}
+      />
+      <input 
+        type="number"
+        value={num2}
+        onChange={(e) => setNum2(e.target.value)}
+      /> <br />
+      <button onClick={() => setResult(h.HandleAdd(num1, num2))}>+</button>
+      <button onClick={() => setResult(h.HandleSub(num1, num2))}>-</button>
+      <button onClick={() => setResult(h.HandleMul(num1, num2))}>*</button>
+      <button onClick={() => setResult(h.HandleDiv(num1, num2))}>/</button> <br />
+
+      <button onClick={clear}>Clear</button>
+      <p>Result: {Number(result).toFixed(2)}</p>
+    </>
+  )
+}
+
+export default App
---
+++ src/components/Handle.jsx
@@ -0,0 +1,15 @@
+export function HandleAdd(num1, num2) {
+    return Number(num1) + Number(num2);
+}
+
+export function HandleSub(num1, num2) {
+    return Number(num1) - Number(num2);
+}
+
+export function HandleMul(num1, num2) {
+    return Number(num1) * Number(num2);
+}
+
+export function HandleDiv(num1, num2) {
+    return Number(num1) / Number(num2);
+}
